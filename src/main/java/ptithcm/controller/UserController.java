cd104d0aa5a302ee4af7fe999e8643be759f7c41
daylahaoo <dohunghao.work@gmail.com>
2024-01-02 12:49:03 +0700

    Tạo UserController

---
+++ src/main/java/ptithcm/controller/UserController.java
@@ -0,0 +1,14 @@
+package ptithcm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user/")
+public class UserController {
+	@RequestMapping("login")
+	public String UserLogin() {
+		
+		return "user/login";
+	}
+}
