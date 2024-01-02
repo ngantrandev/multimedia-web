209a512160c88fd4c1dc12a31913ef05d239b239
Ngan Tran <78363797+ngantrandev@users.noreply.github.com>
2024-01-02 14:46:31 +0700

    add addtocart method

--- src/main/java/ptithcm/controller/AnPhamController.java
+++ src/main/java/ptithcm/controller/AnPhamController.java
@@ -12,25 +12,28 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import ptithcm.entity.AnPham;
+import ptithcm.entity.Order;
 
 @Controller
 @RequestMapping("/anpham/")
 public class AnPhamController {
-	
+
 	@Autowired
 	public SessionFactory sessionFactory;
 
 	@Transactional
-	@RequestMapping ("/show_list")
-	public String showForm(ModelMap model) {
+	@RequestMapping("/show_list")
+	public String showForm(ModelMap model, HttpServletRequest request) {
 		List<AnPham> listAnPham = sessionFactory.getCurrentSession().createQuery("FROM AnPham").list();
-		
+		List<Order> listOrder = sessionFactory.getCurrentSession().createQuery("FROM Order").list();
+
 		model.addAttribute("listAnPham", listAnPham);
+		model.addAttribute("listOrder", listOrder);
+		request.setAttribute("orderSize", listOrder.size());
 
 //		List<Student> notifications = sessionFactory.getCurrentSession().createQuery("FROM Notification").list();
-		System.out.println(listAnPham);
-		
-		
+//		System.out.println(listAnPham);
+
 //		System.out.println(notifications);
 
 		return "anpham/index";
